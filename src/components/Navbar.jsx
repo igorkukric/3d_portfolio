525e42d2879c5d3cdd41cf2d828f862e888e56be
igorkukric <you@example.com>
2023-09-28 03:11:49 +0200

    add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,9 +23,8 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Igor <span className="sm:block hidden">| Kukric </span>
-          </p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Igor &nbsp; <span className="sm:block hidden">| Kukric</span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
