3c0af61fded645a9bf4fbcb9e567810d0a2ac85d
Igor Kukric <igorkukric@gmail.com>
2023-09-30 00:33:35 +0200

    add HOC , update About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -21,7 +22,7 @@ const ServiceCard = ({ index, title, icon }) => {
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
-          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <img src={icon} alt="web-development" className="w-16 h-16 object-contain" />
           <h3 className="text-white text-[20px] font-bold text-center">
             {title}{" "}
           </h3>
@@ -59,4 +60,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about")
