ac5a816f8552e4d1406953782a4ffdd580b02f89
Igor_Kukric <igorkukric@gmail.com>
2023-10-01 00:02:16 +0200

    fix Computers UI and Ball UI

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,17 +8,17 @@ const Computers = ({isMobile}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={6.15} groundColor="black" />
       
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={6}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={6} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
