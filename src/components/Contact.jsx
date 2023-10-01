d4e2d31de72da338ea2e72b9414cabadc9a06f40
Igor_Kukric <igorkukric@gmail.com>
2023-10-01 23:00:45 +0200

    import ande create structure for Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [loading, setLoading] = useState(false)
 
-export default Contact
+  const handleChange = (e) => {}
+
+  const handleSubmit = () => {}
+
+  return <div>Contact</div>;
+};
+
+export default Contact;
